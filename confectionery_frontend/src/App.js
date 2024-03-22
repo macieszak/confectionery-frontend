897c42828d762da1e6b5187c61cad1551a6f1d0d
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-03-22 20:13:18 +0100

    Adding URL links to navigation bar.

--- confectionery_frontend/src/App.js
+++ confectionery_frontend/src/App.js
@@ -1,12 +1,36 @@
 import './App.css'
 import NavigationBar from "./components/navigationBar/NavigationBar";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs.jsx';
+import Product from './pages/Product.jsx';
+import Contact from './pages/Contact.jsx';
+import Cart from './pages/Cart.jsx';
+import LoginSignUp from './pages/LoginSignUp.jsx'
+import User from './pages/User';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <NavigationBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/aboutus' element={<AboutUs/>}/>
+        <Route path='/products' element={<Product/>}/>
+        <Route path='/contacts' element={<Contact/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/> 
+        </Route>
+         <Route path='/cart' element={<Cart/>}/>
+         <Route path='/user' element={<User/>}/>
+         <Route path='/login' element={<LoginSignUp/>}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
--- confectionery_frontend/src/components/navigationBar/NavigationBar.jsx
+++ confectionery_frontend/src/components/navigationBar/NavigationBar.jsx
@@ -3,6 +3,7 @@ import './NavigationBar.css'
 import logo from '../../assets/logo.png'
 import cart_logo from '../../assets/cart.png'
 import user_logo from '../../assets/user.svg'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
 
@@ -14,15 +15,15 @@ const NavigationBar = () => {
             <img src={logo} alt="website logo" width={100} height={100}/>
         </div>
     <ul className='nav-menu'>
-        <li onClick={() => {setMenu("home")}}>HOME {menu == "home" ? <hr /> :<></>} </li>
-        <li onClick={() => {setMenu("about us")}}>ABOUT US {menu == "about us" ? <hr /> :<></>}</li>
-        <li onClick={() => {setMenu("product")}}>PRODUCT {menu == "product" ? <hr /> :<></>}</li>
-        <li onClick={() => {setMenu("contact")}}>CONTACT {menu == "contact" ? <hr /> :<></>}</li>
+        <li onClick={() => {setMenu("home")}}><Link style= {{textDecoration:'none'}} to='/'>HOME</Link> {menu == "home" ? <hr /> :<></>} </li>
+        <li onClick={() => {setMenu("about us")}}><Link style= {{textDecoration:'none'}} to='/aboutus'>ABOUT US</Link>{menu == "about us" ? <hr /> :<></>}</li>
+        <li onClick={() => {setMenu("product")}}><Link style= {{textDecoration:'none'}} to='/products'>PRODUCT</Link> {menu == "product" ? <hr /> :<></>}</li>
+        <li onClick={() => {setMenu("contact")}}><Link style= {{textDecoration:'none'}} to='/contacts'>CONTACT</Link> {menu == "contact" ? <hr /> :<></>}</li>
     </ul>
     <div className='nav-login-cart'>
-        <button>LOGIN</button>
-        <img src={user_logo} alt="user logo" />
-        <img src={cart_logo} alt="cart logo"/>
+    <Link to='/login'><button>LOGIN</button></Link>
+    <Link to='/user'><img src={user_logo} alt="user logo"/></Link>
+    <Link to='/cart'><img src={cart_logo} alt="cart logo"/></Link>
         <div className="nav-cart-count">0</div>
         <select name="language" id="language">
             <option value="EN">EN</option>
