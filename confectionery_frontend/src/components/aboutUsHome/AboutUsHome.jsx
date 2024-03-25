5546ebd3965429caf45709ad093ad4ffffc5f501
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-03-25 23:36:26 +0100

    Added about us info to the home page.

---
+++ confectionery_frontend/src/components/aboutUsHome/AboutUsHome.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './AboutUsHome.css'
+import { Link } from 'react-router-dom'
+
+const AboutUsHome = ({ setMenu }) => {
+  return (
+    <div className='about-us-home'>
+     
+      <div className='about-us-info'>
+        <h1>About Us</h1>
+        <p>Welcome to Sweet Bakery, where every bite tells a story of tradition, passion, and the finest ingredients. Established in the heart of the city, our bakery has been serving up delectable treats made from family recipes passed down through generations since 1985. Our commitment to quality and craftsmanship shines through in every pastry, cake, and cookie we create, making life's moments even sweeter. Join us on a journey of flavor that celebrates the joy of baking and the community we've built around it.</p>
+      </div>
+      
+      <div className='about-us-more-information'>
+        <h1>Do you want to know more information about us?</h1>
+        <Link to='/aboutus'><button onClick={() => setMenu('about us')} >Check it out!</button></Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default AboutUsHome
--- confectionery_frontend/src/pages/Home.jsx
+++ confectionery_frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import MainInfo from '../components/MainInfo/MainInfo'
+import MainInfo from '../components/mainInfo/MainInfo'
+import AboutUsHome from '../components/aboutUsHome/AboutUsHome'
 
 const Home = ({setMenu}) => {
   return (
     <div>
       <MainInfo setMenu={setMenu}/>
+      <AboutUsHome setMenu={setMenu}/>
     </div>
   )
 }
