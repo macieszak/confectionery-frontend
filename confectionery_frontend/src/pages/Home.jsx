fae6acd78ea3ad74a42f8c8b6042d19a9fc31e5f
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-03-26 13:39:55 +0100

    Added contact info to the home page.

--- confectionery_frontend/src/pages/Home.jsx
+++ confectionery_frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainInfo from '../components/mainInfo/MainInfo'
 import AboutUsHome from '../components/aboutUsHome/AboutUsHome'
+import NewsLetter from '../components/NewsLetter/NewsLetter'
 
 const Home = ({setMenu}) => {
   return (
     <div>
       <MainInfo setMenu={setMenu}/>
       <AboutUsHome setMenu={setMenu}/>
+      <NewsLetter/>
     </div>
   )
 }
