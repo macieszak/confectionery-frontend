3149ba25ec4741464e3da7aa32a77e9217bf32e2
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-04-25 09:49:01 +0200

    Updating the admin account with sorting, filtering and searching for products.

--- confectionery_frontend/src/pages/pages_admin/AdminProducts.jsx
+++ confectionery_frontend/src/pages/pages_admin/AdminProducts.jsx
@@ -12,9 +12,105 @@ const AdminProducts = () => {
 	const [searchTerm, setSearchTerm] = useState('')
 	const [categoryFilter, setCategoryFilter] = useState('all')
 	const [priceFilter, setPriceFilter] = useState('all')
-	
+
 	const navigate = useNavigate()
 	const { user } = useAuth(); 
+
+	//
+	const fetchFilteredProducts = () => {
+		const params = {
+			category: categoryFilter !== 'all' ? categoryFilter : null,
+			minPrice: priceFilter === '0-15' ? 0 : (priceFilter === '15-50' ? 15 : null),
+			maxPrice: priceFilter === '0-15' ? 15 : (priceFilter === '15-50' ? 50 : null)
+		};
+	
+		axios.get(`http://localhost:8080/api/user/products/filter`, { params })
+			.then(response => {
+				setProducts(response.data.map(product => ({
+					...product,
+					imageUrl: product.image.name
+				})));
+			})
+			.catch(error => {
+				console.error('Error fetching filtered products', error);
+			});
+	};
+
+
+	const handleSortChange = (e) => {
+		const sortValue = e.target.value;
+		setSortOption(sortValue);
+	
+		fetchSortedProducts(sortValue);
+	};
+	
+	const fetchSortedProducts = (sortOption) => {
+		axios.get(`http://localhost:8080/api/user/products/sorted`, {
+			params: { sort: sortOption }
+		})
+		.then(response => {
+			setProducts(response.data.map(product => ({
+				...product,
+				imageUrl: product.image.name  
+			})));
+		})
+		.catch(error => {
+			console.log('Error fetching sorted products', error);
+		});
+	};
+
+	const fetchAllProducts = () => {
+		axios.get('http://localhost:8080/api/user/products/all')
+			.then(response => {
+				setProducts(response.data.map(product => ({
+					...product,
+					imageUrl: product.image.name  
+				})));
+			})
+			.catch(error => {
+				console.log('Error fetching all products', error);
+			});
+	};
+	
+
+	useEffect(() => {
+		if (searchTerm) {
+			fetchProductsBySearch(searchTerm);
+		} else {
+			// Możesz zdecydować, czy chcesz wyświetlić wszystkie produkty, gdy nie ma wyszukiwania
+			fetchAllProducts();
+		}
+	}, [searchTerm]);
+
+	const fetchProductsBySearch = (query) => {
+		axios.get(`http://localhost:8080/api/user/products/search`, {
+			params: { query }
+		})
+		.then(response => {
+			setProducts(response.data.map(product => ({
+				...product,
+				imageUrl: product.image.name  
+			})));
+		})
+		.catch(error => {
+			console.log('Error fetching products by search', error);
+		});
+	};
+
+
+	useEffect(() => {
+		fetchSortedProducts(sortOption);
+	}, [sortOption]);  // To gwarantuje, że każda zmiana opcji sortowania pobierze dane na nowo
+
+	useEffect(() => {
+		fetchFilteredProducts();
+	}, [categoryFilter, priceFilter]);  // Zareaguj na zmianę filtrów
+
+
+	//
+
+
+
 
 	useEffect(() => {
         axios.get('http://localhost:8080/api/admin/products/all')
